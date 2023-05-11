f62d37f9c110b88039504341f8decd1c7348dc2e
Hongyi Peng <hongyi001@e.ntu.edu.sg>
2023-05-11 13:49:10 +0800

    modified: datasets.py

--- federate_simulator/datasets.py
+++ federate_simulator/datasets.py
@@ -5,6 +5,7 @@ from abc import ABC, abstractmethod
 from box import Box
 from .utils import load_dataset
 
+
 class BaseFederateDataWrapper(ABC):
     def __init__(self, config:Box, **kwargs):
         self.config = config
@@ -19,7 +20,7 @@ class BaseFederateDataWrapper(ABC):
     def on_global_round_start(self):
         pass
     @abstractmethod
-    def on_global_round_end(self):
+    def on_global_round_end(self): 
         pass
     
 
@@ -28,10 +29,11 @@ class FederatedDatasetWrapper(BaseFederateDataWrapper):
     def __init__(self, config:Box, **kwargs):
         self.cfg = config.dataset
         self.dataset = self.cfg.dataset
-        self.trainset, self.testset, self.valset = load_dataset(self.dataset)
-        if (type(self.trainset) == torch.utils.data.Dataset 
-            and type(self.testset) == torch.utils.data.Datase 
-            and type(self.valset) == torch.utils.data.Dataset):
+        trainset, testset, valset = load_dataset(self.dataset, config.dataset)
+        # if the dataset return is centralized, we need to split it
+        if (type(trainset) == torch.utils.data.Dataset 
+            and type(testset) == torch.utils.data.Dataset 
+            and type(valset) == torch.utils.data.Dataset):
             self.num_clients = self.cfg.num_clients
             self.batch_size = self.cfg.batch_size
             self.split_type = self.cfg.split_type
@@ -42,17 +44,30 @@ class FederatedDatasetWrapper(BaseFederateDataWrapper):
                 self.splitter = LDASplitter(self.num_clients, **kwargs)
             elif self.split_type == 'iid':
                 self.splitter = IIDSplitter(self.num_clients, **kwargs)
-            self.train_idx_mapping = self.splitter(self.trainset).get_clients_dataidx_map()
+            self.train_idx_mapping = self.splitter(trainset).get_clients_dataidx_map()
             # In case, the setting require local test and local val dataset
             if self.cfg.require_local_test:
-                self.test_idx_mapping = self.splitter(self.testset).get_clients_dataidx_map()
+                self.test_idx_mapping = self.splitter(testset).get_clients_dataidx_map()
             if self.cfg.require_local_val:
-                self.val_idx_mapping = self.splitter(self.valset).get_clients_dataidx_map()
+                self.val_idx_mapping = self.splitter(valset).get_clients_dataidx_map()
+        # Real dataset is already splitted into clients idx maps
+        elif (type(trainset)== ClientDataIdxMap 
+              and type(testset) == ClientDataIdxMap
+              and type(valset) == ClientDataIdxMap):
+            self.train_idx_mapping = trainset
+            self.test_idx_mapping = testset
+            self.val_idx_mapping = valset
             
-     
-            
-            
-        
+    def on_global_round_start(self):
+        pass
+    
+    def on_local_round_start(self, client_id):
+        trainset = Subset(self.dataset, self.train_idx_mapping[client_id])
+        self.client_loader = DataLoader(trainset, batch_size=self.batch_size, shuffle=True)
+        return self.client_loader
+    
+    def on_local_round_end(self):
+        if 
 
     def split_dataset(self):
         self.client_indices = self.splitter.get_clients_dataidx_map()
